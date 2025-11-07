8ce6fc409e92efb65325fe6056eecb079c7607b5
Mizuki Nguyen <49761211+MagicalDrizzle@users.noreply.github.com>
2025-11-08 02:53:03 +0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -62,8 +62,9 @@ dnf5 install -y gparted gsmartcontrol btdu btrfs-heatmap \
                 android-tools java-21-openjdk usbview podman-compose \
                 cascadia-fonts-all coolercontrol playerctl cmus \
                 kitty konsole ksystemlog byobu golly ucblogo ddccontrol ddccontrol-gtk \
-                rmlint cava vkmark iotop powertop \
-                plasma-workspace-x11
+                rmlint cava vkmark iotop powertop
+                # plasma-workspace-x11
+                # installing this crash the whole system
 dnf5 install -y --setopt=install_weak_deps=False plasma-discover \
                         plasma-discover-flatpak plasma-discover-kns
 # Use a COPR Example:
